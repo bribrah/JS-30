5d5ac5e1efd489af10b7d6d7a8cb0df398fbcae3
bribrah <cheesemas46@gmail.com>
2019-03-10 16:46:32 -0700

    finished canvas exercise

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let projectListHTML = '';
-for (i = 2; i<=7;i++){
+for (i = 2; i<=8;i++){
         projectListHTML += `<div class="project"><a href="${i}/index.html"><div class="caption">Day ${i}
         </div><img src="${i}/${i}.png"></a></div>`;
 }
