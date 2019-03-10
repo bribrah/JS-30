4f6cc8548fb73692a64838564a48a824723cb82b
bribrah <cheesemas46@gmail.com>
2019-03-10 15:42:06 -0700

    better index page

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let projectListHTML = '';
 for (i = 2; i<=7;i++){
-        projectListHTML += `<li><a href="${i}/index.html">Day ${i}</a></li>`;
+        projectListHTML += `<div class="project"><a href="${i}/index.html"><div class="caption">Day ${i}
+        </div><img src="${i}/${i}.png"></a></div>`;
 }
 
-const projectListDiplay = document.querySelector('.projects-list');
+const projectListDiplay = document.querySelector('.projects');
 projectListDiplay.innerHTML = projectListHTML;
